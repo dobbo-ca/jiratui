416a6ba7822db3fc2066c8ce250e14ed636d1487
Chris Dobbyn <8264498+cdobbyn@users.noreply.github.com>
2026-04-01 23:12:37 -0700

    feat: add Jira API response types for v3 REST API

---
+++ internal/jira/types.go
@@ -0,0 +1,119 @@
+package jira
+
+// jiraSearchResponse is the response from /rest/api/3/search
+type jiraSearchResponse struct {
+	StartAt    int         `json:"startAt"`
+	MaxResults int         `json:"maxResults"`
+	Total      int         `json:"total"`
+	Issues     []jiraIssue `json:"issues"`
+}
+
+type jiraIssue struct {
+	Key    string     `json:"key"`
+	Self   string     `json:"self"`
+	Fields jiraFields `json:"fields"`
+}
+
+type jiraFields struct {
+	Summary     string           `json:"summary"`
+	Description *jiraADF         `json:"description"`
+	Status      jiraStatus       `json:"status"`
+	Priority    *jiraPriority    `json:"priority"`
+	IssueType   jiraIssueType    `json:"issuetype"`
+	Assignee    *jiraUser        `json:"assignee"`
+	Reporter    *jiraUser        `json:"reporter"`
+	Labels      []string         `json:"labels"`
+	Created     string           `json:"created"`
+	Updated     string           `json:"updated"`
+	DueDate     *string          `json:"duedate"`
+	Parent      *jiraParent      `json:"parent"`
+	Sprint      *jiraSprint      `json:"sprint"`
+	Subtasks    []jiraIssue      `json:"subtasks"`
+	IssueLinks  []jiraIssueLink  `json:"issuelinks"`
+	Comment     *jiraCommentPage `json:"comment"`
+	Project     jiraProject      `json:"project"`
+}
+
+// jiraADF represents Atlassian Document Format. We extract plain text from it.
+type jiraADF struct {
+	Type    string     `json:"type"`
+	Content []jiraNode `json:"content"`
+}
+
+type jiraNode struct {
+	Type    string     `json:"type"`
+	Text    string     `json:"text,omitempty"`
+	Content []jiraNode `json:"content,omitempty"`
+}
+
+type jiraStatus struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type jiraPriority struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type jiraIssueType struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type jiraUser struct {
+	AccountID   string            `json:"accountId"`
+	DisplayName string            `json:"displayName"`
+	Email       string            `json:"emailAddress"`
+	AvatarURLs  map[string]string `json:"avatarUrls"`
+}
+
+type jiraParent struct {
+	Key    string `json:"key"`
+	Fields struct {
+		Summary string     `json:"summary"`
+		Status  jiraStatus `json:"status"`
+	} `json:"fields"`
+}
+
+type jiraSprint struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type jiraIssueLink struct {
+	ID   string `json:"id"`
+	Type struct {
+		Name    string `json:"name"`
+		Inward  string `json:"inward"`
+		Outward string `json:"outward"`
+	} `json:"type"`
+	InwardIssue  *jiraLinkedIssue `json:"inwardIssue"`
+	OutwardIssue *jiraLinkedIssue `json:"outwardIssue"`
+}
+
+type jiraLinkedIssue struct {
+	Key    string `json:"key"`
+	Fields struct {
+		Summary string     `json:"summary"`
+		Status  jiraStatus `json:"status"`
+	} `json:"fields"`
+}
+
+type jiraCommentPage struct {
+	Total    int           `json:"total"`
+	Comments []jiraComment `json:"comments"`
+}
+
+type jiraComment struct {
+	ID      string   `json:"id"`
+	Author  jiraUser `json:"author"`
+	Body    *jiraADF `json:"body"`
+	Created string   `json:"created"`
+	Updated string   `json:"updated"`
+}
+
+type jiraProject struct {
+	Key  string `json:"key"`
+	Name string `json:"name"`
+}
